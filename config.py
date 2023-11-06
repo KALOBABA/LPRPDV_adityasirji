00cdeb135ccc0523bfbcd7b26c1ed15fd39be715
adityasirji <146421388+adityasirji@users.noreply.github.com>
2023-11-06 08:15:37 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6756823663:AAFVW3NDF0EB2UsTarqMxVqi0g5_ZjXXs3o")
+    API_ID = int(os.environ["API_ID", 27060147]
+    API_HASH = os.environ["API_HASH", "70f93c98c61a40f69c0f830bd4fece7c"]
+    AUTH_USERS = "6693536228 """
+
